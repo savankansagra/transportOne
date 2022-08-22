27a3dab2fada5b4c21c7938fe428975a1e25ef8d
savan <kansagrasavan108@gmail.com>
2022-08-22 11:50:34 +0530

    let's start

---
+++ src/test/java/com/transport/TransportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
